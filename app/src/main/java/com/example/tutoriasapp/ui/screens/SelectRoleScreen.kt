d18d5d22a1d7648d2680ec72487ed408de3d520c
Gothman240 <paez.federico96@gmail.com>
2026-06-28 20:29:39 -0300

    Refactor

--- app/src/main/java/com/example/tutoriasapp/ui/screens/SelectRoleScreen.kt
+++ app/src/main/java/com/example/tutoriasapp/ui/screens/SelectRoleScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
@@ -51,25 +52,30 @@ fun SelectRoleScreen() {
                 .padding(horizontal = 16.dp, vertical = 36.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Spacer(Modifier.weight(0.5f))
+            Spacer(Modifier.height(46.dp))
             Text("Selecciona tu camino", style = MaterialTheme.typography.headlineLarge, fontWeight = FontWeight.SemiBold)
             Spacer(modifier = Modifier.height(16.dp))
             Box(modifier = Modifier.padding(horizontal = 24.dp)){
-                Text("Seleccione cómo desea utilizar Tutorias para personalizar su experiencia.",
+                Text("Selecciona cómo desea utilizar TutorMe para personalizar su experiencia.",
                     textAlign = TextAlign.Center,
                     style = MaterialTheme.typography.bodyMedium
                 )
             }
-            Spacer(Modifier.height(16.dp))
+            Spacer(Modifier.height(22.dp))
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
+                    .shadow(
+                        elevation = 2.dp,
+                        shape = RoundedCornerShape(36.dp),
+                        clip = false
+                    )
                     .background(Color.White, shape = RoundedCornerShape(36.dp))
             ) {
                 Column(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(horizontal = 66.dp, vertical = 26.dp),
+                        .padding(horizontal = 60.dp, vertical = 26.dp),
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
                     Box(
@@ -95,38 +101,49 @@ fun SelectRoleScreen() {
                         "Necesito apoyo académico y tutorías para sobresalir en mis clases.",
                         textAlign = TextAlign.Center, style = MaterialTheme.typography.bodyMedium
                     )
-                    Spacer(modifier = Modifier.height(16.dp))
-                    Row(verticalAlignment = Alignment.CenterVertically) {
-                        Text(
-                            "Empezar",
-                            color = Color(0xFF0C56D1),
-                            style = MaterialTheme.typography.labelLarge,
+                    Spacer(modifier = Modifier.height(18.dp))
+                }
+                Box(
+                    modifier = Modifier
+                        .padding(bottom = 20.dp, end = 24.dp) // Pequeño margen para que no toque los bordes rígidos
+                        .size(32.dp)
+                        .background(
+                            color = Color(0xFF0C56D1).copy(alpha = .1f), // Fondo azul muy suave
+                            shape = CircleShape
                         )
-                        Icon(painter = painterResource(R.drawable.round_chevron_right_24),
-                            contentDescription = null,
-                            tint = Color(0xFF0C56D1)
-                        )
-                    }
-                    Spacer(modifier = Modifier.height(12.dp))
+                        .align(Alignment.BottomEnd), // 👈 Se posiciona sola abajo a la derecha
+                    contentAlignment = Alignment.Center
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.round_chevron_right_24),
+                        contentDescription = null,
+                        modifier = Modifier.size(20.dp),
+                        tint = Color(0xFF0C56D1) // Tu azul
+                    )
                 }
             }
-            Spacer(Modifier.height(12.dp))
+            Spacer(Modifier.height(22.dp))
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
+                    .shadow(
+                        elevation = 2.dp,
+                        shape = RoundedCornerShape(36.dp),
+                        clip = false
+                    )
                     .background(Color.White, shape = RoundedCornerShape(36.dp))
             ) {
                 Column(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(horizontal = 66.dp, vertical = 26.dp),
+                        .padding(horizontal = 60.dp, vertical = 26.dp),
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
                     Box(
                         modifier = Modifier
                             .size(100.dp)
                             .background(
-                                color = Color(168,57,4).copy(alpha = .1f),
+                                color = Color(242, 107, 34).copy(alpha = .1f),
                                 shape = CircleShape
                             ),
                         contentAlignment = Alignment.Center
@@ -135,7 +152,7 @@ fun SelectRoleScreen() {
                             painterResource(R.drawable.edu),
                             contentDescription = null,
                             modifier = Modifier.size(60.dp),
-                            tint = Color(168,57,4)
+                            tint = Color(242, 107, 34)
                         )
                     }
                     Spacer(modifier = Modifier.height(16.dp))
@@ -145,19 +162,25 @@ fun SelectRoleScreen() {
                         "Quiero ayudar a otros, compartir conocimientos y ganar dinero mientras enseño.",
                         textAlign = TextAlign.Center, style = MaterialTheme.typography.bodyMedium
                     )
-                    Spacer(modifier = Modifier.height(16.dp))
-                    Row(verticalAlignment = Alignment.CenterVertically) {
-                        Text(
-                            "Empezar",
-                            color = Color(168,57,4),
-                            style = MaterialTheme.typography.labelLarge,
+                    Spacer(modifier = Modifier.height(18.dp))
+                }
+                Box(
+                    modifier = Modifier
+                        .padding(bottom = 20.dp, end = 24.dp)
+                        .size(32.dp)
+                        .background(
+                            color = Color(242, 107, 34).copy(alpha = .1f),
+                            shape = CircleShape
                         )
-                        Icon(painter = painterResource(R.drawable.round_chevron_right_24),
-                            contentDescription = null,
-                            tint = Color(168,57,4)
-                        )
-                    }
-                    Spacer(modifier = Modifier.height(12.dp))
+                        .align(Alignment.BottomEnd),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.round_chevron_right_24),
+                        contentDescription = null,
+                        modifier = Modifier.size(20.dp),
+                        tint = Color(242, 107, 34)
+                    )
                 }
             }
         }
