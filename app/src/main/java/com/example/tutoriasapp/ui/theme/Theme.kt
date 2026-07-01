cc7eaa47a2481cbd801ea096b25d9ed219de91af
sadtoru <daiarocio@outlook.com>
2026-07-01 15:36:53 -0300

    Update dark theme

--- app/src/main/java/com/example/tutoriasapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/tutoriasapp/ui/theme/Theme.kt
@@ -35,7 +35,7 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun TutoriasAppTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    //darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
@@ -43,15 +43,15 @@ fun TutoriasAppTheme(
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+            //if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
 
-        darkTheme -> DarkColorScheme
+        //darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
 
     MaterialTheme(
-        colorScheme = colorScheme,
+        //colorScheme = colorScheme,
         typography = Typography,
         content = content
     )
